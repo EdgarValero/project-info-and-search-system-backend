221d1a85349e3d52e23c784f94e7e8c08cf587e9
Edgar Valero <edgaralexandervb@gmail.com>
2019-11-02 18:52:11 -0400

    Agregadas rutas para mostrar, actualizar y eliminar usuarios administradores.

--- src/routes/usersAdmin.routes.js
+++ src/routes/usersAdmin.routes.js
@@ -11,8 +11,8 @@ router.post('/signup', async (req, res) => {
         adminEmail, 
         adminPassword
     }
-    const data = await userAdminController.signUp(input);
-    res.json(data);
+    await userAdminController.signUp(input);
+    res.json({msg: 'user_admin_saved'});
 });
 
 router.post('/signin', async (req, res) => {
@@ -23,6 +23,30 @@ router.post('/signin', async (req, res) => {
     }
     const data = await userAdminController.signIn(input);
     res.json(data);
+});
+
+router.get('/', async (req, res) => {
+    const data = await userAdminController.getUsersAdmin();
+    res.json(data);
+});
+
+router.put('/update/:id', async (req, res) => {
+    const { id } = req.params;
+    const { adminFirstName, adminLastName, adminEmail, adminPassword} = req.body;
+    const input = {
+        adminFirstName,
+        adminLastName,
+        adminEmail,
+        adminPassword
+    };
+    await userAdminController.updateUserAdmin(id, input);
+    res.json({msg: 'user_admin_updated'});
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await userAdminController.deleteUserAdmin(id);
+    res.json({msg: 'user_admin_deleted'});
 });
 
 module.exports = router;
